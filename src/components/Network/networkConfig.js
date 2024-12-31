4d5a3b5a63eb81527df70c08c75853de025a6976
Mustafa-Hasnain <mustafahasnain19@gmail.com>
2024-12-31 21:29:59 +0500

    Clinic Updates

--- src/components/Network/networkConfig.js
+++ src/components/Network/networkConfig.js
@@ -1,2 +1,2 @@
-export const network_url = "http://localhost:5037";
-// export const network_url = "https://mustafa019-001-site1.ntempurl.com"
+// export const network_url = "http://localhost:5037";
+export const network_url = "https://mustafa019-001-site1.ntempurl.com"
